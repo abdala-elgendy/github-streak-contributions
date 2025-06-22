166b941e5368b683e047e504d55dfd99a1b0195e
abdullah <elgendyabdala@gmail.com>
2025-06-22 12:35:43 +0300

    show streakCounter in popup.html

---
+++ popup.js
@@ -0,0 +1,18 @@
+// We will add code here to update the streak count in popup.html
+document.addEventListener("DOMContentLoaded", () => {
+    //get the streak from storage and disply it.
+    const streakCountElement = document.getElementById("streak-count");
+
+    chrome.storage.local.get(["streak"], (result) => {
+        streakCountElement.textContent = result.streak || 0;
+    });
+});
+
+document.getElementById('open-options').addEventListener('click', function(e) {
+  e.preventDefault();
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    window.open(chrome.runtime.getURL('options.html'));
+  }
+});
